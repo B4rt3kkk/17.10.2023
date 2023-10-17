604b73ef2760d8ae66f8b71caec3e248fb483b2d
FilipZembrzuski <fzembrzuski@gmail.com>
2023-10-17 10:30:07 +0200

    2

--- Tablice/Tablice/Tablice.cpp
+++ Tablice/Tablice/Tablice.cpp
@@ -3,8 +3,15 @@ using namespace std;
 //Tablice statystyczne;
 int main()
 {
-	int tablica[5] = { 1, 2, 3, 4, 5 };
-	for (size_t i = 0; i <lenght; i++) 
-
-
+	int tablica[5] = { 5, 10, 15, 20, 25 };
+	for (size_t i = 0; i < 5; i++)
+	{
+		cout <<"index = " << i << " " << tablica[i] << endl;
+	}
+	cout << "---------------------------------------" << endl;
+	for (int i = 4; i >= 0; i--)
+	{
+		cout << "index = " << i << " " << tablica[i] << endl;
+	}
+	return 0;
 }
---
+++ Zadanie2/Zadanie2/Zadanie2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int tablica[] = {10,20,30,1,2};
+    int rozmiarTablicy = sizeof(tablica)/sizeof(int);
+
+    for (int i = 0; i < rozmiarTablicy; i++) {
+        cout << tablica[i] << endl;
+    }
+    // cout << tablica[0] << endl; -> wartość: 10 
+    // cout << tablica[1] << endl; -> wartość: 20 
+    // cout << tablica[2] << endl; -> wartość: 30 
+    // cout << tablica[3] << endl; -> wartość: 1 
+    // cout << tablica[4] << endl; -> wartość: 2 
+    cout << "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_" << endl;
+    for (int i = rozmiarTablicy - 1; i >= 0; i--) {
+        cout << tablica[i] << endl;
+    }
+
+
+}
